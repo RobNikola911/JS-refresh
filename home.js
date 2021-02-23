ad2b1912b3e1541d266e9e5b8b29c0f4135034a2
Robert Pajic <pajic1991@gmail.com>
2021-02-23 13:58:51 +0100

    First commit

---
+++ home.js
@@ -0,0 +1,18 @@
+console.log('hello');
+
+// alert('yooo');
+
+// Variables
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+// Manipulate DOM with JS means changing HTML with JS
+
+var age = prompt('What is your age?');
+
+document.getElementById('someText').innerHTML = age;
+
+// Numbers in JS
