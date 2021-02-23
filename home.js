e19c4af7f6a56db419a5f2c7cf44ce5567ba174d
Robert Pajic <pajic1991@gmail.com>
2021-02-23 18:08:15 +0100

    data types

--- home.js
+++ home.js
@@ -11,8 +11,42 @@ console.log(someNumber);
 
 // Manipulate DOM with JS means changing HTML with JS
 
-var age = prompt('What is your age?');
+// var age = prompt('What is your age?');
 
-document.getElementById('someText').innerHTML = age;
+// document.getElementById('someText').innerHTML = age;
 
 // Numbers in JS
+var num1 = 5.7;
+console.log(5 * 10);
+
+// Functions 
+
+function greeting(yourName) {
+    var result = 'Hello ' + yourName;
+    console.log(result);
+}
+
+var name = prompt('What is your name?');
+greeting(name);
+
+// fun();
+
+// How do arguments work in functions?
+
+function sumNumbers(num1, num2){
+    var result = num1 + num2;
+    console.log(result);
+}
+
+sumNumbers(10, 10);
+
+var num = 0;
+
+while (num < 100) {
+    num += 10;
+    console.log(num);
+}
+
+for (let num = 0; num <= 100; num += 10) {
+    console.log(num);
+}
