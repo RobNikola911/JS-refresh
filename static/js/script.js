7ed76dba047688218a7ca3c8703ecaf322c62007
Robert Pajic <pajic1991@gmail.com>
2021-03-10 23:39:40 +0100

    compute winner logic works

--- static/js/script.js
+++ static/js/script.js
@@ -208,6 +208,262 @@ const buttonsBlue = () => {
   }
 };
 
-
 // Challenge 5: BlackJack!
 
+let blackjackGame = {
+  you: { scoreSpan: "#player-blackjack-result", div: "#player-box", score: 0 },
+  dealer: {
+    scoreSpan: "#dealer-blackjack-result",
+    div: "#dealer-box",
+    score: 0,
+  },
+  cards: [
+    "2S",
+    "3S",
+    "4S",
+    "5S",
+    "6S",
+    "7S",
+    "8S",
+    "9S",
+    "10S",
+    "JS",
+    "QS",
+    "KS",
+    "AS",
+    "2D",
+    "3D",
+    "4D",
+    "5D",
+    "6D",
+    "7D",
+    "8D",
+    "9D",
+    "10D",
+    "JD",
+    "QD",
+    "KD",
+    "AD",
+    "2H",
+    "3H",
+    "4H",
+    "5H",
+    "6H",
+    "7H",
+    "8H",
+    "9H",
+    "10H",
+    "JH",
+    "QH",
+    "KH",
+    "AH",
+    "2C",
+    "3C",
+    "4C",
+    "5C",
+    "6C",
+    "7C",
+    "8C",
+    "9C",
+    "10C",
+    "JC",
+    "QC",
+    "KC",
+    "AC",
+  ],
+  cardsMap: {
+    "2S": 2,
+    "3S": 3,
+    "4S": 4,
+    "5S": 5,
+    "6S": 6,
+    "7S": 7,
+    "8S": 8,
+    "9S": 9,
+    "10S": 10,
+    JS: 10,
+    QS: 10,
+    KS: 10,
+    AS: [1, 11],
+    "2D": 2,
+    "3D": 3,
+    "4D": 4,
+    "5D": 5,
+    "6D": 6,
+    "7D": 7,
+    "8D": 8,
+    "9D": 9,
+    "10D": 10,
+    JD: 10,
+    QD: 10,
+    KD: 10,
+    AD: [1, 11],
+    "2H": 2,
+    "3H": 3,
+    "4H": 4,
+    "5H": 5,
+    "6H": 6,
+    "7H": 7,
+    "8H": 8,
+    "9H": 9,
+    "10H": 10,
+    JH: 10,
+    QH: 10,
+    KH: 10,
+    AH: [1, 11],
+    "2C": 2,
+    "3C": 3,
+    "4C": 4,
+    "5C": 5,
+    "6C": 6,
+    "7C": 7,
+    "8C": 8,
+    "9C": 9,
+    "10C": 10,
+    JC: 10,
+    QC: 10,
+    KC: 10,
+    AC: [1, 11],
+  },
+};
+
+const YOU = blackjackGame["you"];
+const DEALER = blackjackGame["dealer"];
+
+const hitSound = new Audio("static/sounds/swish.m4a");
+const winSound = new Audio("static/sounds/cash.mp3");
+const lossSound = new Audio("static/sounds/aww.mp3");
+
+document
+  .querySelector("#blackjack-hit-button")
+  .addEventListener("click", blackjackHit);
+
+function blackjackHit() {
+  let card = randomCard();
+  showCard(card, YOU);
+  updateScore(card, YOU);
+  showScore(YOU);
+}
+
+function randomCard() {
+  let randomIndex = Math.floor(Math.random() * 52);
+  return blackjackGame["cards"][randomIndex];
+}
+
+function showCard(card, activePlayer) {
+  if (activePlayer["score"] <= 21) {
+    let cardImage = document.createElement("img");
+    cardImage.src = `static/images/${card}.png`;
+    document.querySelector(activePlayer["div"]).appendChild(cardImage);
+    hitSound.play();
+  }
+}
+
+function updateScore(card, activePlayer) {
+  if (card === "AS" || card === "AH" || card === "AD" || card === "AC") {
+    if (activePlayer["score"] + blackjackGame["cardsMap"][card][1] <= 21) {
+      activePlayer["score"] += blackjackGame["cardsMap"][card][1];
+    } else {
+      activePlayer["score"] += blackjackGame["cardsMap"][card][0];
+    }
+  } else {
+    activePlayer["score"] += blackjackGame["cardsMap"][card];
+  }
+}
+
+function showScore(activePlayer) {
+  if (activePlayer["score"] > 21) {
+    document.querySelector(activePlayer["scoreSpan"]).textContent = "Too Many!";
+    document.querySelector(activePlayer["scoreSpan"]).style.color = "red";
+  } else {
+    document.querySelector(activePlayer["scoreSpan"]).textContent =
+      activePlayer["score"];
+  }
+}
+
+document
+  .querySelector("#blackjack-deal-button")
+  .addEventListener("click", blackjackDeal);
+
+function blackjackDeal() {
+  showResult(computeWinnder());
+
+  let yourImages = document
+    .querySelector("#player-box")
+    .querySelectorAll("img");
+
+  for (i = 0; i < yourImages.length; i++) {
+    yourImages[i].remove();
+  }
+
+  let dealerImages = document
+    .querySelector("#dealer-box")
+    .querySelectorAll("img");
+
+  for (i = 0; i < dealerImages.length; i++) {
+    dealerImages[i].remove();
+  }
+
+  YOU["score"] = 0;
+  DEALER["score"] = 0;
+
+  document.querySelector("#player-blackjack-result").textContent = 0;
+  document.querySelector("#player-blackjack-result").style.color = "white";
+  document.querySelector("#dealer-blackjack-result").textContent = 0;
+  document.querySelector("#dealer-blackjack-result").style.color = "white";
+}
+
+document
+  .querySelector("#blackjack-stand-button")
+  .addEventListener("click", blackjackStand);
+
+function blackjackStand() {
+  let card = randomCard();
+  showCard(card, DEALER);
+  updateScore(card, DEALER);
+  showScore(DEALER);
+}
+
+function computeWinnder() {
+  let winner;
+
+  if (YOU["score"] <= 21) {
+    if (YOU["score"] > DEALER["score"] || DEALER["score"] > 21) {
+      console.log("You won!");
+      winner = YOU;
+    } else if (YOU["score"] < DEALER["score"]) {
+      console.log("You lost!");
+      winner = DEALER;
+    } else if (YOU["score"] === DEALER["score"]) {
+      console.log("Push!");
+    }
+  } else if (YOU["score"] > 21 && DEALER["score"] <= 21) {
+    console.log("You lost!");
+    winner = DEALER;
+  } else if (YOU["score"] > 21 && DEALER["score"] > 21) {
+    console.log("You drew!");
+  }
+
+  console.log("Winner is ", winner);
+  return winner;
+}
+
+function showResult(winner) {
+  let message, messageColor;
+
+  if (winner === YOU) {
+    message = "You won!";
+    messageColor = "green";
+    winSound.play();
+  } else if (winner === DEALER) {
+    message = "You lost!";
+    messageColor = "red";
+    lossSound.play();
+  } else {
+    message = "You drew!";
+    messageColor = "black";
+  }
+
+  document.querySelector("#blackjack-result").textContent = message;
+  document.querySelector("#blackjack-result").style.color = messageColor;
+}
